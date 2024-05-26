03caab6a92d1efb557f4ced66a4ef00fc19005f6
Leo Jay Magistrado <ljballanomagistrado020@gmail.com>
2024-05-26 10:48:09 +0800

    finished task

--- 03_php_pdo_mysql_connection.php
+++ 03_php_pdo_mysql_connection.php
@@ -1,16 +1,16 @@
 <?php
 
 $host = 'localhost';
-$db = 'school-database';
+$db = 'school_database';
 $user = 'root';
-$password = 'mypassword';
+$password = 'your_password';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=UTF8";
-//$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXEPTION];
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 
 try {
 
-    $pdo = new PDO($dsn , $user , $password);
+    $pdo = new PDO($dsn , $user , $password, $options);
 
     if($pdo){
         echo "Connected to the $db Database Successfuly" ;
